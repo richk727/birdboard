ef158e21afe9f62adbe5f6b0c71f11d4f7078d25
Richard Kaye <richk727@gmail.com>
2019-10-31 23:30:32 +0000

    Initial project management set up

---
+++ app/Http/Controllers/ProjectsController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Project;
+use Illuminate\Http\Request;
+
+class ProjectsController extends Controller
+{
+    public function index()
+    {
+        $projects = auth()->user()->projects;
+        //$projects = Project::all();
+        return view('projects.index', compact('projects'));
+    }
+
+    public function show(Project $project)
+    {
+        if(auth()->user()->isNot($project->owner) ) {
+            abort(403);
+        }
+        return view('projects.show', compact('project'));
+    }
+
+    public function create(Project $project)
+    {
+        return view('projects.create');
+    }
+
+    public function store()
+    {
+        $attributes = request()->validate([
+            'title' => 'required',
+            'description' => 'required'
+        ]);
+
+        auth()->user()->projects()->create($attributes);
+        
+        return redirect('/projects');
+    }
+}
---
+++ resources/views/projects/create.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Birdboard</title>
+</head>
+<body>
+    <h1>Create a project</h1>
+
+    <form method="POST" action="/projects">
+        @csrf 
+        <div>
+            <label for="title">Title</label>
+            <input type="text" name="title" id="title">
+        </div>
+        <div>
+            <label for="description">Description</label>
+            <textarea name="description" id="description" cols="30" rows="10"></textarea>
+        </div>
+        <div><button type="submit">Create Project</button></div>
+    </form>
+</body>
+</html>
